bccdc417c47607529d8fcab1a8f0eb1a70fdeff0
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-07-01 17:53:09 +0800

    home page style complete

---
+++ src/Pages/Home/Home.component.jsx
@@ -0,0 +1,9 @@
+import './Home.styles.scss'
+import Directory from '../../Components/Direcotry/Directory.component'
+export default function Home() {
+	return (
+	<div className="homepage">
+		<Directory/>
+	</div>
+	);
+}

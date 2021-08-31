615fdde1e3b21779d6bd06375dcbf3ecc251ec56
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-08-31 14:21:22 +0800

    ui fix

--- src/Pages/Contact/Contact.component.tsx
+++ src/Pages/Contact/Contact.component.tsx
@@ -4,9 +4,8 @@ const Contact:React.FC = () => {
 
 	return (<div className="contact-content">
 		<div className="contact-info">
-		<h2 className="contact-title">Contact Information</h2>
 			<div className="contact-name">Designer: Mr. Jiang</div>
-			<div className="contact-email"><a href="mailto:qh15.jiang@gmail.com"><strong>Email To me</strong></a></div>
+			{/* <div className="contact-email"><a href="mailto:qh15.jiang@gmail.com"><strong>Email To me</strong></a></div> */}
 			</div>
 	</div>)
 }
